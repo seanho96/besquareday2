9cc05b01b6559ebac3c9875294074114e111b294
Sean Ho <seanho96@yahoo.com>
2021-05-27 23:23:12 +0800

    setup day 2 page

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,15 +14,12 @@ const headingStyles = {
 const headingAccentStyles = {
   color: "#663399",
 }
+const sectionHeadingStyles = {
+  marginTop: 80,
+  marginBottom: 32,
+}
 const paragraphStyles = {
   marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
 }
 const listStyles = {
   marginBottom: 96,
@@ -56,11 +53,11 @@ const descriptionStyle = {
   lineHeight: 1.25,
 }
 
-const docLink = {
-  text: "Documentation",
-  url: "https://www.gatsbyjs.com/docs/",
-  color: "#8954A8",
-}
+// const docLink = {
+//   text: "Documentation",
+//   url: "https://www.gatsbyjs.com/docs/",
+//   color: "#8954A8",
+// }
 
 const badgeStyle = {
   color: "#fff",
@@ -78,52 +75,182 @@ const badgeStyle = {
   lineHeight: 1,
 }
 
-// data
-const links = [
+const sass_links = [
   {
-    text: "Tutorial",
-    url: "https://www.gatsbyjs.com/docs/tutorial/",
+    text: "Nesting",
+    url: "https://codepen.io/seanho96/pen/wvoMPGv",
     description:
-      "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
+      "",
     color: "#E95800",
   },
   {
-    text: "How to Guides",
-    url: "https://www.gatsbyjs.com/docs/how-to/",
+    text: "Functions",
+    url: "https://codepen.io/seanho96/pen/OJpWEXq",
     description:
-      "Practical step-by-step guides to help you achieve a specific goal. Most useful when you're trying to get something done.",
+      "",
     color: "#1099A8",
   },
   {
-    text: "Reference Guides",
-    url: "https://www.gatsbyjs.com/docs/reference/",
+    text: "Mixins",
+    url: "https://codepen.io/seanho96/pen/YzpwEQg",
     description:
-      "Nitty-gritty technical descriptions of how Gatsby works. Most useful when you need detailed information about Gatsby's APIs.",
+      "",
     color: "#BC027F",
   },
   {
-    text: "Conceptual Guides",
-    url: "https://www.gatsbyjs.com/docs/conceptual/",
+    text: "Extend",
+    url: "https://codepen.io/seanho96/pen/YzpwEQg",
     description:
-      "Big-picture explanations of higher-level Gatsby concepts. Most useful for building understanding of a particular topic.",
+      "",
     color: "#0D96F2",
   },
+  // {
+  //   text: "Plugin Library",
+  //   url: "https://www.gatsbyjs.com/plugins",
+  //   description:
+  //     "Add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
+  //   color: "#8EB814",
+  // },
+  // {
+  //   text: "Build and Host",
+  //   url: "https://www.gatsbyjs.com/cloud",
+  //   badge: true,
+  //   description:
+  //     "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
+  //   color: "#663399",
+  // },
+]
+
+const rwd_links = [
   {
-    text: "Plugin Library",
-    url: "https://www.gatsbyjs.com/plugins",
+    text: "Why do we need responsive design?",
+    url: "https://www.smashingmagazine.com/2011/01/guidelines-for-responsive-web-design/",
     description:
-      "Add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
+      "",
+    color: "#E95800",
+  },
+  {
+    text: "Viewport",
+    url: "https://codepen.io/matini94/pen/QWdKzYZ",
+    description:
+      "",
+    color: "#1099A8",
+  },
+  {
+    text: "Media Queries",
+    url: "https://codepen.io/matini94/pen/RwogEoo?editors=0010",
+    description:
+      "",
+    color: "#BC027F",
+  },
+  {
+    text: "Responsive Media",
+    url: "https://codepen.io/matini94/pen/RwogEoo?editors=0010",
+    description:
+      "",
+    color: "#0D96F2",
+  },
+   {
+    text: "Grid Layout",
+    url: "https://codepen.io/matini94/pen/LYWLMmw",
+    description:
+      "",
     color: "#8EB814",
   },
   {
-    text: "Build and Host",
-    url: "https://www.gatsbyjs.com/cloud",
+    text: "Flexbox",
+    url: "https://codepen.io/matini94/pen/dyvRwdb",
+    description:
+      "",
+    color: "#663399",
+  },
+  {
+    text: "Multiple-column Layout",
+    url: "https://codepen.io/matini94/pen/oNBZdQR",
+    description:
+      "",
+    color: "#1099A8",
+  },
+   {
+    text: "Grid Garden",
+    url: "https://cssgridgarden.com/",
     badge: true,
     description:
-      "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
+      "",
+    color: "#8EB814",
+  },
+  {
+    text: "Flexbox Froggy",
+    url: "https://flexboxfroggy.com/",
+    badge: true,
+    description:
+      "",
     color: "#663399",
   },
 ]
+
+const animation_links = [
+  {
+    text: "2D/3D Transform",
+    url: "https://codepen.io/seanho96/pen/BaQoaEZ",
+    description:
+      "",
+    color: "#E95800",
+  },
+  {
+    text: "Task 1",
+    url: "https://codepen.io/seanho96/pen/bGBjvGE",
+    description:
+      "",
+    color: "#1099A8",
+  },
+  {
+    text: "Transition",
+    url: "https://codepen.io/seanho96/pen/QWGjBJq",
+    description:
+      "",
+    color: "#BC027F",
+  },
+  {
+    text: "Task 2",
+    url: "https://codepen.io/seanho96/pen/NWbBYNr",
+    description:
+      "",
+    color: "#0D96F2",
+  },
+   {
+    text: "Animation",
+    url: "https://codepen.io/matini94/pen/YzpWOdP",
+    description:
+      "",
+    color: "#8EB814",
+  },
+  {
+    text: "Species in Pieces",
+    url: "http://www.species-in-pieces.com/",
+    badge: true,
+    description:
+      "",
+    color: "#663399",
+  },
+  {
+    text: "Your Plan Your Planet",
+    url: "https://yourplanyourplanet.sustainability.google/",
+    badge: true,
+    description:
+      "",
+    color: "#1099A8",
+  },
+   {
+    text: "One Design Company",
+    url: "https://onedesigncompany.com/",
+    badge: true,
+    description:
+      "",
+    color: "#8EB814",
+  },
+]
+
 
 // markup
 const IndexPage = () => {
@@ -131,41 +258,89 @@ const IndexPage = () => {
     <main style={pageStyles}>
       <title>Home Page</title>
       <h1 style={headingStyles}>
-        Congratulations
+        Welcome to
         <br />
-        <span style={headingAccentStyles}>— you just made a Gatsby site! </span>
+        <span style={headingAccentStyles}>BeSquare Day 2! </span>
         <span role="img" aria-label="Party popper emojis">
-          🎉🎉🎉
+          🎉🎉
         </span>
       </h1>
       <p style={paragraphStyles}>
-        Edit <code style={codeStyles}>src/pages/index.js</code> to see this page
-        update in real-time.{" "}
+        Please find some of the links and resources we'll be covering today here on this page{" "}
         <span role="img" aria-label="Sunglasses smiley emoji">
           😎
         </span>
       </p>
       <ul style={listStyles}>
         <li style={docLinkStyle}>
-          <a
-            style={linkStyle}
-            href={`${docLink.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-          >
-            {docLink.text}
-          </a>
+          <h2 style={sectionHeadingStyles}>
+            SASS
+          </h2>
         </li>
-        {links.map(link => (
+        {sass_links.map(link => (
           <li key={link.url} style={{ ...listItemStyles, color: link.color }}>
             <span>
               <a
                 style={linkStyle}
-                href={`${link.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
+                href={link.url}
+                rel="noopener noreferrer"
+                target="_blank"
               >
                 {link.text}
               </a>
               {link.badge && (
                 <span style={badgeStyle} aria-label="New Badge">
-                  NEW!
+                  HOT!
+                </span>
+              )}
+              <p style={descriptionStyle}>{link.description}</p>
+            </span>
+          </li>
+        ))}
+        <li style={docLinkStyle}>
+          <h2 style={sectionHeadingStyles}>
+            Responsive Web Design
+          </h2>
+        </li>
+        {rwd_links.map(link => (
+          <li key={link.url} style={{ ...listItemStyles, color: link.color }}>
+            <span>
+              <a
+                style={linkStyle}
+                href={link.url}
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                {link.text}
+              </a>
+              {link.badge && (
+                <span style={badgeStyle} aria-label="New Badge">
+                  HOT!
+                </span>
+              )}
+              <p style={descriptionStyle}>{link.description}</p>
+            </span>
+          </li>
+        ))}
+        <li style={docLinkStyle}>
+          <h2 style={sectionHeadingStyles}>
+            Animation
+          </h2>
+        </li>
+        {animation_links.map(link => (
+          <li key={link.url} style={{ ...listItemStyles, color: link.color }}>
+            <span>
+              <a
+                style={linkStyle}
+                href={link.url}
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                {link.text}
+              </a>
+              {link.badge && (
+                <span style={badgeStyle} aria-label="New Badge">
+                  HOT!
                 </span>
               )}
               <p style={descriptionStyle}>{link.description}</p>
@@ -173,10 +348,6 @@ const IndexPage = () => {
           </li>
         ))}
       </ul>
-      <img
-        alt="Gatsby G Logo"
-        src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
-      />
     </main>
   )
 }
